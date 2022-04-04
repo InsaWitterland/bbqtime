c67ac4bba38f0dfd8e1bb3419c31501480480856
OrosC <orosschoks@gmail.com>
2022-04-04 21:43:26 +0200

    style grill and sate

--- Eindopdracht/script/script.js
+++ Eindopdracht/script/script.js
@@ -1,3 +1,10 @@
 function startGame()    {
     
 }
+
+function addCookingToSate(){
+    let plate = document.getElementById("plate");
+    let sate1 = document.getElementById("sate1");
+
+    #TODO when we click on plate we want to add the css sateCooking to element sate1
+}
